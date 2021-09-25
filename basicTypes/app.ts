b9fcf5b1d685363110c7a451765659d923b6cea8
julienshim <25723428+julienshim@users.noreply.github.com>
2021-09-24 22:44:33 -0700

    basic types init

---
+++ basicTypes/app.ts
@@ -0,0 +1,22 @@
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    // console.log(typeof n1)
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //     throw new Error('Incorrect input!');
+    // }
+    const result = n1 + n2
+    if(showResult) {
+        console.log(phrase + result);
+    } else {
+        return result
+    }
+}
+
+const number1 = 5; // all numbers are floats by default
+const number2 = 2.8;
+const printResult = true
+const resultPhrase = 'Result is: '
+
+// const result = add(number1, number2, printResult);
+// console.log(result);
+
+add(number1, number2, printResult, resultPhrase);
